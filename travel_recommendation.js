8c70a4ba77b3fbbc7bd6090f4c71400f014d9766
Dhivshan <dhivyalshan@gmail.com>
2024-11-19 13:18:03 -0500

    Added new images and updated files

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,30 +1,56 @@
   function searchCondition() {
         const input = document.getElementById('conditionInput').value.toLowerCase();
-        const resultDiv = document.getElementById('result');
-        if(input=="country" || input=="countries" || input=="australia"|| input=="japan" || input=="brazil"){
+        const resultDiv = document.getElementById('popup-results');
+        if(input=="country" || input=="countries" ){   
         resultDiv.innerHTML = '';
-        resultDiv.innerHTML += `<h2>${input}</h2>`;
-        resultDiv.innerHTML += `<img src="country.jpg">`;
-        resultDiv.innerHTML += `<p><strong>Name:</strong> ${input}</p>`;
-        resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A lively place known for its stunning beaches, vibrant carnival celebrations, and iconic landmarks"}</p>`;
-    
-        }else if(input=="temple" || input=="temples" || input=="cambodia"|| input=="india"){
+        resultDiv.innerHTML += `<h2>${"Australia"}</h2>`;
+
+        resultDiv.innerHTML += `<img src="syd_img.jpg">`;
+        resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Sydney, Australia"}</p>`;
+        resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A vibrant city known for its iconic landmarks like the Sydney Opera House and Sydney Harbour Bridge."}</p>`;
+
+        resultDiv.innerHTML += `<h2>${"Japan"}</h2>`;
+
+        resultDiv.innerHTML += `<img src="tok_img.jpg">`;
+        resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Tokyo, Japan"}</p>`;
+        resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A bustling metropolis blending tradition and modernity, famous for its cherry blossoms and rich culture."}</p>`;
+
+       
+    } else if(input=="temple" || input=="temples"){
+        resultDiv.innerHTML = '';
+        resultDiv.innerHTML += `<h2>${"Cambodia"}</h2>`;
+
+        resultDiv.innerHTML += `<img src="Angkor_templ_img.jpg">`;
+        resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Angkor Wat, Cambodia"}</p>`;
+        resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A UNESCO World Heritage site and the largest religious monument in the world."}</p>`;
+
+        resultDiv.innerHTML += `<h2>${"India"}</h2>`;
+
+        resultDiv.innerHTML += `<img src="taj_templ_img.jpg">`;
+        resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Taj Mahal, India"}</p>`;
+        resultDiv.innerHTML += `<p><strong>Description:</strong> ${"An iconic symbol of love and a masterpiece of Mughal architecture."}</p>`;
+
+        }else if(input=="beach"|| input=="beaches"){
             resultDiv.innerHTML = '';
-            resultDiv.innerHTML += `<h2>${input}</h2>`;
-            resultDiv.innerHTML += `<img src="taj_templ_img.jpg">`;
-            resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Taj Mahal, India"}</p>`;
-            resultDiv.innerHTML += `<p><strong>Description:</strong> ${"An iconic symbol of love and a masterpiece of Mughal architecture."}</p>`;
-            }else if(input=="beach"|| input=="beaches" || input=="french polynesia"|| input=="brazil"){
-                resultDiv.innerHTML = '';
-                resultDiv.innerHTML += `<h2>${input}</h2>`;
-                resultDiv.innerHTML += `<img src="copacabana_beach.jpg">`;
-                resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Copacabana Beach, Brazil"}</p>`;
-                resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A famous beach in Rio de Janeiro, Brazil, with a vibrant atmosphere and scenic views"}</p>`;
+            resultDiv.innerHTML += `<h2>${"French Polynesia"}</h2>`;
+    
+            resultDiv.innerHTML += `<img src="bora_beach.jpg">`;
+            resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Bora Bora, French Polynesia"}</p>`;
+            resultDiv.innerHTML += `<p><strong>Description:</strong> ${"An island known for its stunning turquoise waters and luxurious overwater bungalows."}</p>`;
+    
+            resultDiv.innerHTML += `<h2>${"Brazil"}</h2>`;
+    
+            resultDiv.innerHTML += `<img src="copacabana_beach.jpg">`;
+            resultDiv.innerHTML += `<p><strong>Name:</strong> ${"Copacabana Beach, Brazil"}</p>`;
+            resultDiv.innerHTML += `<p><strong>Description:</strong> ${"A famous beach in Rio de Janeiro, Brazil, with a vibrant atmosphere and scenic views."}</p>`;
                 }else{
 
                     resultDiv.innerHTML = '';
                     resultDiv.innerHTML += `<h2>${"Results not found"}</h2>`;   
                 }
+                // Show the popup
+                const popup = document.getElementById("search-popup");
+                popup.classList.remove("hidden");
       }
         btnSearch.addEventListener('click', searchCondition);
 
